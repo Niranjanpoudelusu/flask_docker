3919cc9459cc6b7d8864ef894fb62e0fc32f7091
Niranjan <niranjan111@hotmail.com>
2024-04-13 00:27:21 -0700

    first commit

---
+++ app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, send_file
+from plotdata import regression_plot
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+def regr_plot():
+    image = regression_plot()
+
+    return send_file(image,
+    download_name='regplot.png',
+    mimetype = 'image/png' )
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',debug = False)
+
